92d5bf4ae4a7d28b0462c4264aee8f07e2d75768
nikul6 <nikuldhameliya6@gmail.com>
2023-08-22 20:03:03 +0530

    Animation added

--- src/components/RingProgress.tsx
+++ src/components/RingProgress.tsx
@@ -1,6 +1,9 @@
 import { StyleSheet, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import SVG, { Circle } from 'react-native-svg';
+import Animated, { useAnimatedProps, useSharedValue, withTiming } from 'react-native-reanimated';
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 type RingProgressProps = {
   redius?: number;
@@ -13,6 +16,17 @@ const color = '#EE0F55';
 export default function RingProgress({ redius = 100, strokeWidth = 35, progress }: RingProgressProps) {
   const innerRedius = redius - strokeWidth / 2;
   const cirumference = 2 * Math.PI * innerRedius;
+
+  const fill = useSharedValue(0);
+
+  useEffect(() => {
+    fill.value = withTiming(progress);
+  }, []);
+
+  const animatedProps = useAnimatedProps(() => ({
+    strokeDasharray: [cirumference * fill.value, cirumference],
+  }))
+
   return (
     <View style={{
       width: redius * 2,
@@ -29,7 +43,8 @@ export default function RingProgress({ redius = 100, strokeWidth = 35, progress 
           stroke={color}
           opacity={0.2}
         />
-        <Circle
+        <AnimatedCircle
+          animatedProps={animatedProps}
           r={innerRedius}
           cx={redius}
           cy={redius}
